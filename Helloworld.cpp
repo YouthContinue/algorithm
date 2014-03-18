7b3f49897853b3901dafd55ed102ab7326041993
YouthContinue <YouthContinue@users.noreply.github.com>
2014-03-18 10:56:55 +0800

    Update Helloworld.cpp
    add some changes test

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 
-void main()
+int main()
 {
 	cout<<"just a github tese"<<endl;
 	cout<<"Hello world"<<endl;
+	
+	for (int i = 0; i < 100; i++){
+		cout<<"i = %d"<<i<<endl;
+	}
 }
